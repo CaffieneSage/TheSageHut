ea7786599e80ddc0920ed32566328dfb69dc5ea0
TCS <caffeinesage44@gmail.com>
2026-06-29 21:40:33 +0100

    made the mobile nav bar close after a link is pressed

--- src/components/MobileNavigation.vue
+++ src/components/MobileNavigation.vue
@@ -1,19 +1,20 @@
 <template>
-  <div class="mobile-navigation">
-    <button class="hamburger" @click="open = !open">
-      ☰
-    </button>
+    <div class="mobile-navigation">
+        <button class="hamburger" @click="open = !open">
+            ☰
+        </button>
 
-    <div v-if="open" class="mobile-menu">
-      <ul class="nav-links">
-      <li><router-link to="/">Home</router-link></li>
-      <li><router-link to="/youtube">Youtube</router-link></li>
-      <li><router-link to="/about">About</router-link></li>
-      <li><router-link to="/support">Support Me</router-link></li>
-      <li><router-link to="/privacy">Privacy</router-link></li>
-    </ul>
+        <div v-if="open" class="mobile-menu">
+            <ul class="nav-links">
+                <li><router-link to="/" @click="open = false">Home</router-link></li>
+                <li><router-link to="/youtube" @click="open = false">Youtube</router-link></li>
+                <li><router-link to="/about" @click="open = false">About</router-link></li>
+                <li><router-link to="/support" @click="open = false">Support Me</router-link></li>
+                <li><router-link to="/privacy" @click="open = false">Privacy</router-link></li>
+
+            </ul>
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup>
@@ -23,47 +24,47 @@ const open = ref(false)
 
 <style scoped>
 .mobile-nav {
-  display: none;
+    display: none;
 }
 
 .hamburger {
-  font-size: 28px;
-  background: none;
-  border: none;
-  padding: 10px;
-  cursor: pointer;
-  color: #fff;
+    font-size: 28px;
+    background: none;
+    border: none;
+    padding: 10px;
+    cursor: pointer;
+    color: #fff;
 }
 
 .mobile-menu {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 70%;
-  height: 100vh;
-  background: #111;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  border-right: 3px solid #ff7a00;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 70%;
+    height: 100vh;
+    background: #111;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    border-right: 3px solid #ff7a00;
 }
 
 .mobile-menu a {
-  color: #fff;
-  font-size: 20px;
+    color: #fff;
+    font-size: 20px;
 }
 
 @media (max-width: 768px) {
-  .mobile-nav {
-    display: flex;
-    align-items: center;
-    padding: 10px;
-    background: #111;
-    border-bottom: 3px solid #ff7a00;
-    position: sticky;
-    top: 0;
-    z-index: 999;
-  }
+    .mobile-nav {
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        background: #111;
+        border-bottom: 3px solid #ff7a00;
+        position: sticky;
+        top: 0;
+        z-index: 999;
+    }
 }
 </style>
