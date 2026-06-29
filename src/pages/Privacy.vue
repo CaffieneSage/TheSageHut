00f26f6e57b8bd2f7bcfe1fc90c616a79c8eea1f
TCS <caffeinesage44@gmail.com>
2026-06-29 21:37:37 +0100

    Updated the privacy statement to reflect analytics being enabled

--- src/pages/Privacy.vue
+++ src/pages/Privacy.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="Privacy">
     <h1>Privacy</h1>
-    <p>This is the Privacy page.</p>
+    <p>This site is hosted by Vercel and tracks some anonymous analytics,
+       such as how many people visited and very limited information regarding where they came
+       from. This information is deleted after 30 days.</p>
   </div>
 </template>
 
